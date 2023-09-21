cbf516eee4eab8e1c7f887d5adc6040647a7ed2a
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-09-21 19:11:05 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/demo/Application.java
@@ -0,0 +1,15 @@
+package com.otekin.dev.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.otekin.dev.demo.repository")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
